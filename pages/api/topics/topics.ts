8985376d482e209325c98728add6ecd55bfc176a
Gustavo Grossi <grossigus@gmail.com>
2023-07-09 15:15:28 -0300

    returned nextApiRequest

--- pages/api/topics/topics.ts
+++ pages/api/topics/topics.ts
@@ -1,7 +1,10 @@
-export default async function handler(res: NextApiResponse) {
-  const response = await fetch(
-    `https://api.explodingtopics.com/api/v1/topics?pro=false&type=all&categories=crypto&sort=date_added&order=asc&timeframe=180&limit=100&response_timeframe=last_12_months&api_key=${process.env.EXPLODING_TOPICS_API_KEY}`
-  );
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await fetch(`https://api.explodingtopics.com/api/v1/topics?pro=false&type=all&categories=crypto&sort=date_added&order=asc&timeframe=180&limit=100&response_timeframe=last_12_months&api_key=${process.env.EXPLODING_TOPICS_API_KEY}`);
 
   const data = await response.json();
 
