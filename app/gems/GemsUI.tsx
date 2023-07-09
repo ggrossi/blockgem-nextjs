20552dd2f0fa0ed5df31b66893bdd9c0aa0b8039
Gustavo Grossi <grossigus@gmail.com>
2023-07-09 15:06:21 -0300

    Integrated ET API to Gems

--- app/gems/GemsUI.tsx
+++ app/gems/GemsUI.tsx
@@ -1,9 +1,61 @@
-// pages/articles/GemsUI.tsx
+'use client';
+
+import { useEffect, useState } from 'react';
+
 export default function GemsUI() {
-    return (
-      <div>
-        <h1>Hello World Gems.</h1>
-      </div>
-    );
+  const [topics, setTopics] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    fetch('/api/topics/topics')
+      .then((response) => response.json())
+      .then((data) => {
+        if (isMounted) {
+          setTopics(data.result);
+          setLoading(false);
+        }
+      })
+      .catch((error) => {
+        if (isMounted) {
+          setError(error.message);
+          setLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-  
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {Array.isArray(topics) &&
+        topics.map((topic, index) => (
+          <div key={index} className="p-4 border rounded-md shadow-lg">
+            <h2 className="text-xl font-bold mb-2">{topic.keyword}</h2>
+            <p className="text-gray-700 mb-2">{topic.description}</p>
+            <p className="text-sm text-gray-500 mb-1">
+              Search Volume: {topic.absolute_volume}
+            </p>
+            <p className="text-sm text-gray-500">
+              Growth (6 months): {topic.search_history?.growth['6']}%
+            </p>
+            <p className="text-sm text-gray-500">
+              Growth (3 months): {topic.search_history?.growth['3']}%
+            </p>
+          </div>
+        ))}
+    </div>
+  );
+}
---
+++ pages/api/topics/topics.ts
@@ -0,0 +1,12 @@
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await fetch(
+    `https://api.explodingtopics.com/api/v1/topics?pro=false&type=all&categories=crypto&sort=date_added&order=asc&timeframe=180&limit=100&response_timeframe=last_12_months&api_key=${process.env.EXPLODING_TOPICS_API_KEY}`
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
