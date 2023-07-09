2ad448a7816ab364f6dec0158f52f6d1536955cc
Gustavo Grossi <grossigus@gmail.com>
2023-07-09 15:11:30 -0300

    adjusted topics state on Gems

--- app/gems/GemsUI.tsx
+++ app/gems/GemsUI.tsx
@@ -3,9 +3,22 @@
 import { useEffect, useState } from 'react';
 
 export default function GemsUI() {
-  const [topics, setTopics] = useState([]);
+  const [topics, setTopics] = useState<Topic[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  
+  interface Topic {
+    keyword: string;
+    description: string;
+    absolute_volume: number;
+    search_history: {
+      growth: {
+        '6': number;
+        '3': number;
+      };
+    };
+  }
+  
 
   useEffect(() => {
     let isMounted = true;
@@ -40,22 +53,15 @@ export default function GemsUI() {
 
   return (
     <div className="grid grid-cols-3 gap-4">
-      {Array.isArray(topics) &&
-        topics.map((topic, index) => (
-          <div key={index} className="p-4 border rounded-md shadow-lg">
-            <h2 className="text-xl font-bold mb-2">{topic.keyword}</h2>
-            <p className="text-gray-700 mb-2">{topic.description}</p>
-            <p className="text-sm text-gray-500 mb-1">
-              Search Volume: {topic.absolute_volume}
-            </p>
-            <p className="text-sm text-gray-500">
-              Growth (6 months): {topic.search_history?.growth['6']}%
-            </p>
-            <p className="text-sm text-gray-500">
-              Growth (3 months): {topic.search_history?.growth['3']}%
-            </p>
-          </div>
-        ))}
+      {topics.map((topic: Topic, index: number) => (
+        <div key={index} className="p-4 border rounded-md shadow-lg">
+          <h2 className="text-xl font-bold mb-2">{topic.keyword}</h2>
+          <p className="text-gray-700 mb-2">{topic.description}</p>
+          <p className="text-sm text-gray-500 mb-1">Absolute Volume: {topic.absolute_volume}</p>
+          <p className="text-sm text-gray-500">Growth (6 months): {topic.search_history?.growth['6']}</p>
+          <p className="text-sm text-gray-500">Growth (3 months): {topic.search_history?.growth['3']}</p>
+        </div>
+      ))}
     </div>
   );
 }
