7b0ebdde8246666b2f43ab16db6ae00fabbaf37c
Gustavo Grossi <grossigus@gmail.com>
2023-07-10 14:08:33 -0300

    rewind v2

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -2,40 +2,29 @@ import { getSession, getSubscription } from '@/app/supabase-server';
 import { redirect } from 'next/navigation';
 import Navbar from '@/components/ui/Navbar';
 import ArticlesUI from './ArticlesUI';
-import getPosts from '@/utils/blog/getPosts';
 
-export default function ArticlesPage({ allPosts, session, subscription }) {
-  return (
-    <>
-      <Navbar user={session?.user || null} subscription={subscription} />
-      <ArticlesUI allPosts={allPosts} />
-    </>
-  );
-}
-
-export async function getServerSideProps() {
+export default async function ArticlesPage() {
   const session = await getSession();
+
+  console.log('Session:', session); // Add this line
+
+  if (!session) {
+    return redirect('/signin');
+  }
+
   const subscription = await getSubscription();
   const isPayingCustomer = !!subscription;
 
-  if (!session || !isPayingCustomer) {
-    return {
-      redirect: {
-        destination: !session ? '/signin' : '/account',
-        permanent: false,
-      },
-    };
+  console.log('Subscription:', subscription); // Add this line
+
+  if (!isPayingCustomer) {
+    return redirect('/account');
   }
 
-  const allPosts = await getPosts();
-  console.log({ allPosts, session, subscription });
-
-  return {
-    props: {
-      allPosts,
-      session,
-      subscription,
-    },
-  };
+  return (
+    <>
+      <Navbar user={session?.user || null} subscription={subscription} />
+      <ArticlesUI />
+    </>
+  );
 }
-
