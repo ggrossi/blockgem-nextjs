14a91fb83e8c8b60ab20192c6310a0bcba0f472c
Gustavo Grossi <grossigus@gmail.com>
2023-07-10 22:40:41 -0300

    changed supabase provider on navbar

--- components/ui/Navbar/NavbarClient.tsx
+++ components/ui/Navbar/NavbarClient.tsx
@@ -7,6 +7,7 @@ import Logo from '@/components/icons/Logo';
 import SignOutButton from './SignOutButton';
 
 import s from './Navbar.module.css';
+import SupabaseProvider from '@/app/supabase-provider';
 
 interface NavbarClientProps {
   user: User | null;
@@ -20,28 +21,30 @@ export default function NavbarClient({
   const isPayingCustomer = !!subscription;
 
   return (
-    <nav className={s.root}>
-      <a href="#skip" className="sr-only focus:not-sr-only">
-        Skip to content
-      </a>
-      <div className="max-w-6xl px-6 mx-auto">
-        <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
-          <div className="flex items-center flex-1">
-            <Link href="/">
-              <Logo className={s.logo} aria-label="Logo" />
-            </Link>
-            <nav className="hidden ml-6 space-x-2 lg:block">
-              <Link href="/">Pricing</Link>
-              {user && <Link href="/account">Account</Link>}
-              {isPayingCustomer && <Link href="/articles">Articles</Link>}
-              {isPayingCustomer && <Link href="/gems">Gems</Link>}
-            </nav>
-          </div>
-          <div className="flex justify-end flex-1 space-x-8">
-            {user ? <SignOutButton /> : <Link href="/signin">Sign in</Link>}
+    <SupabaseProvider>
+      <nav className={s.root}>
+        <a href="#skip" className="sr-only focus:not-sr-only">
+          Skip to content
+        </a>
+        <div className="max-w-6xl px-6 mx-auto">
+          <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
+            <div className="flex items-center flex-1">
+              <Link href="/">
+                <Logo className={s.logo} aria-label="Logo" />
+              </Link>
+              <nav className="hidden ml-6 space-x-2 lg:block">
+                <Link href="/">Pricing</Link>
+                {user && <Link href="/account">Account</Link>}
+                {isPayingCustomer && <Link href="/articles">Articles</Link>}
+                {isPayingCustomer && <Link href="/gems">Gems</Link>}
+              </nav>
+            </div>
+            <div className="flex justify-end flex-1 space-x-8">
+              {user ? <SignOutButton /> : <Link href="/signin">Sign in</Link>}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </SupabaseProvider>
   );
 }
---
+++ md-articles/helloworld2.md
@@ -0,0 +1,6 @@
+---
+title: 'Hello World 2'
+date: '2023-07-10'
+---
+
+This is my first blog post. Welcome to my blog!
