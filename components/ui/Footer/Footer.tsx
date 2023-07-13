f7275594d1bc303a1c6c0eddf75b4ee8e1b2aa84
Gustavo Grossi <grossigus@gmail.com>
2023-07-13 08:18:48 -0300

    Tweaked footer

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -12,10 +12,10 @@ export default function Footer() {
             href="/"
             className="flex items-center flex-initial font-bold md:mr-24"
           >
-            <span className="mr-2 border rounded-full border-zinc-700">
+            <span className="mr-2">
               <Logo />
             </span>
-            <span>ACME</span>
+            <span>Blockgem</span>
           </Link>
         </div>
         <div className="col-span-1 lg:col-span-2">
@@ -30,26 +30,18 @@ export default function Footer() {
             </li>
             <li className="py-3 md:py-0 md:pb-4">
               <Link
-                href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
-              >
-                About
-              </Link>
-            </li>
-            <li className="py-3 md:py-0 md:pb-4">
-              <Link
-                href="/"
-                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
-              >
-                Careers
-              </Link>
-            </li>
-            <li className="py-3 md:py-0 md:pb-4">
-              <Link
-                href="/"
+                href="https://blog.blockgem.co"
                 className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
               >
                 Blog
+              </Link>
+            </li>
+            <li className="py-3 md:py-0 md:pb-4">
+              <Link
+                href="/signin"
+                className="text-white transition duration-150 ease-in-out hover:text-zinc-200"
+              >
+                Get Started
               </Link>
             </li>
           </ul>
@@ -79,32 +71,12 @@ export default function Footer() {
             </li>
           </ul>
         </div>
-        <div className="flex items-start col-span-1 text-white lg:col-span-6 lg:justify-end">
-          <div className="flex items-center h-10 space-x-6">
-            <a
-              aria-label="Github Repository"
-              href="https://github.com/vercel/nextjs-subscription-payments"
-            >
-              <GitHub />
-            </a>
-          </div>
-        </div>
       </div>
       <div className="flex flex-col items-center justify-between py-12 space-y-4 md:flex-row bg-zinc-900">
         <div>
           <span>
-            &copy; {new Date().getFullYear()} ACME, Inc. All rights reserved.
+            &copy; {new Date().getFullYear()} Blockgem Inc. All rights reserved.
           </span>
-        </div>
-        <div className="flex items-center">
-          <span className="text-white">Crafted by</span>
-          <a href="https://vercel.com" aria-label="Vercel.com Link">
-            <img
-              src="/vercel.svg"
-              alt="Vercel.com Logo"
-              className="inline-block h-6 ml-4 text-white"
-            />
-          </a>
         </div>
       </div>
     </footer>
