c146bae68c62c1f2ee75ec8d0eda22e85e2bdb70
Gustavo Grossi <grossigus@gmail.com>
2023-07-14 00:53:12 -0300

    changed account section

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -73,9 +73,6 @@ export default async function Account() {
             <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
               Account
             </h1>
-            <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-              We partnered with Stripe for a simplified billing.
-            </p>
           </div>
         </div>
         <div className="p-4">
@@ -94,70 +91,6 @@ export default async function Account() {
               ) : (
                 <Link href="/">Choose your plan</Link>
               )}
-            </div>
-          </Card>
-          <Card
-            title="Your Name"
-            description="Please enter your full name, or a display name you are comfortable with."
-            footer={
-              <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
-                <p className="pb-4 sm:pb-0">64 characters maximum</p>
-                <Button
-                  variant="slim"
-                  type="submit"
-                  form="nameForm"
-                  disabled={true}
-                >
-                  {/* WARNING - In Next.js 13.4.x server actions are in alpha and should not be used in production code! */}
-                  Update Name
-                </Button>
-              </div>
-            }
-          >
-            <div className="mt-8 mb-4 text-xl font-semibold">
-              <form id="nameForm" action={updateName}>
-                <input
-                  type="text"
-                  name="name"
-                  className="w-1/2 p-3 rounded-md bg-zinc-800"
-                  defaultValue={userDetails?.full_name ?? ''}
-                  placeholder="Your name"
-                  maxLength={64}
-                />
-              </form>
-            </div>
-          </Card>
-          <Card
-            title="Your Email"
-            description="Please enter the email address you want to use to login."
-            footer={
-              <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
-                <p className="pb-4 sm:pb-0">
-                  We will email you to verify the change.
-                </p>
-                <Button
-                  variant="slim"
-                  type="submit"
-                  form="emailForm"
-                  disabled={true}
-                >
-                  {/* WARNING - In Next.js 13.4.x server actions are in alpha and should not be used in production code! */}
-                  Update Email
-                </Button>
-              </div>
-            }
-          >
-            <div className="mt-8 mb-4 text-xl font-semibold">
-              <form id="emailForm" action={updateEmail}>
-                <input
-                  type="text"
-                  name="email"
-                  className="w-1/2 p-3 rounded-md bg-zinc-800"
-                  defaultValue={user ? user.email : ''}
-                  placeholder="Your email"
-                  maxLength={64}
-                />
-              </form>
             </div>
           </Card>
         </div>
