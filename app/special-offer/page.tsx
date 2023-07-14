e8a915d56768dd822fc804304b8f319a074f34f6
Gustavo Grossi <grossigus@gmail.com>
2023-07-14 16:53:23 -0300

    Removed Pricing from special offer

--- app/special-offer/page.tsx
+++ app/special-offer/page.tsx
@@ -1,29 +1,11 @@
 import SpecialOffer from './SpecialOffer';
-import Pricing from '@/components/Pricing';
 import Navbar from '@/components/ui/Navbar';
-import {
-  getSession,
-  getSubscription,
-  getActiveProductsWithPrices
-} from '@/app/supabase-server';
 
 export default async function OfferPage() {
-  const [session, products, subscription] = await Promise.all([
-    getSession(),
-    getActiveProductsWithPrices(),
-    getSubscription()
-  ]);
-
   return (
     <>
       <Navbar user={session?.user || null} subscription={subscription} />
       <SpecialOffer />
-      <Pricing
-        session={session}
-        user={session?.user}
-        products={products}
-        subscription={subscription}
-      />
     </>
   );
 }
