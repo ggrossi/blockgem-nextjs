1c04fecb7d3c60d607691e25fa22f66c4ce29c9f
Gustavo Grossi <grossigus@gmail.com>
2023-07-14 16:54:29 -0300

    Adjusted Navbar on Special Offer

--- app/special-offer/page.tsx
+++ app/special-offer/page.tsx
@@ -1,7 +1,16 @@
 import SpecialOffer from './SpecialOffer';
 import Navbar from '@/components/ui/Navbar';
+import {
+  getSession,
+  getSubscription
+} from '@/app/supabase-server';
 
 export default async function OfferPage() {
+  const [session, subscription] = await Promise.all([
+    getSession(),
+    getSubscription()
+  ]);
+
   return (
     <>
       <Navbar user={session?.user || null} subscription={subscription} />
