ef7cb40a49d219d920e5d7e770562e0518642466
Gustavo Grossi <grossigus@gmail.com>
2023-07-16 22:04:05 -0300

    changed iframe properties

--- app/articles/xrp/XRPReport.tsx
+++ app/articles/xrp/XRPReport.tsx
@@ -137,11 +137,6 @@ export default function XRPReport() {
                 src="https://widget.coinlib.io/widget?type=chart&theme=dark&coin_id=619&pref_coin_id=1505"
                 width="100%"
                 height="536px"
-                scrolling="auto"
-                marginwidth="0"
-                marginheight="0"
-                frameborder="0"
-                border="0"
                 style={{
                   border: '0',
                   margin: '0',
